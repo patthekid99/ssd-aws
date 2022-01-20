f7e9b543ab1ca6a338d2903da8b1dc8fb68ebdec
patthekid99 <pattyvan4@gmail.com>
2022-01-20 15:08:40 -0800

    Add react app

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
-
 app.use(express.json())
+app.use(express.static("build"))
 
 const pokemons = [
   {
@@ -23,6 +23,10 @@ app.post("/api/pokemons", (req, res) => {
   pokemons.push(data)
   res.send(data)
 })
+
+app.get('*', (req, res) => {
+  res.sendFile('build/index.html');
+});
 
 const port = process.env.PORT || 8080
 app.listen(port, () => console.log(`listening on port ${port}`))
